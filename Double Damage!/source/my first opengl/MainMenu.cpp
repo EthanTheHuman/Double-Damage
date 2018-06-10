00f322aff2487251681183d886ac6809412b2110
EthanTheHuman <32722553+EthanTheHuman@users.noreply.github.com>
2018-06-11 10:19:31 +1200

    Added final stuffs
    Added sphere class
    Added UIsprite class and implemented
    added UI stuff to main menu

--- Double Damage!/source/my first opengl/MainMenu.cpp
+++ Double Damage!/source/my first opengl/MainMenu.cpp
@@ -8,6 +8,7 @@ void MainMenu::Init()
 {
 	MyCamera = new Camera(glm::vec3(0, 0, -3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 	SpriteShader = shaderloader.CreateProgram("Shaders/Sprite.vs", "Shaders/Sprite.fs");
+	UISpriteShader = shaderloader.CreateProgram("Shaders/UISprite.vs", "Shaders/UISprite.fs");
 	ModelBasicShader = shaderloader.CreateProgram("Shaders/ModelBasic.vs", "Shaders/ModelBasic.fs");
 	AmbientShader = shaderloader.CreateProgram("Shaders/Ambient.vs", "Shaders/Ambient.fs");
 	TextShader = shaderloader.CreateProgram("Shaders/Text.vs", "Shaders/Text.fs");
@@ -36,6 +37,14 @@ void MainMenu::Init()
 	TempLabel = new TextLabel("Back", "fonts/arial.ttf", glm::vec2(600, 50));
 	TempLabel->SetColor(glm::vec3(1.0f, 1.0f, 0.2f));
 	playMenu.push_back(TempLabel);
+
+	Title = new UISprite("Textures/Cubemap/Title.png", MyCamera, UISpriteShader);
+	Title->SetScale({ 0.3, 0.3, 0.3 });
+	Title->SetTranslation({ 0, 0.5, 0 });
+
+	Controls = new UISprite("Textures/Controls.png", MyCamera, UISpriteShader);
+	Controls->SetScale({ 1, 1, 1 });
+	Controls->SetTranslation({ 0, 0, 0 });
 
 	_Player = new Player(MyCamera, AmbientShader);
 	MySkybox = new CubeMap(MyCamera, SkyboxShader, "Space/bkg1_top.png", "Space/bkg1_bot.png", "Space/bkg1_right.png", "Space/bkg1_left.png", "Space/bkg1_front.png", "Space/bkg1_back.png");
@@ -68,10 +77,12 @@ void MainMenu::Render()
 	//Background
 	MySkybox->Render();
 	_Player->Render();
+
 	if (menu == MAIN) {
 		for (int i = 0; i < mainMenu.size(); i++) {
 			mainMenu[i]->Render();
 		}
+		Title->render();
 	}
 	else if (menu == PLAY) {
 		for (int i = 0; i < playMenu.size(); i++) {
@@ -79,7 +90,7 @@ void MainMenu::Render()
 		}
 	}
 	else if (menu == CONTROL) {
-
+		Controls->render();
 	}
 }
 
@@ -138,7 +149,7 @@ void MainMenu::MoveCharacter(unsigned char KeyState[255]) {
 				MenuUpdate();
 			}
 			else if (selection == 2) {
-				//exit
+				exit(0);
 			}
 		}
 		else if (menu == PLAY) {
@@ -154,6 +165,15 @@ void MainMenu::MoveCharacter(unsigned char KeyState[255]) {
 				selection = 0;
 				MenuUpdate();
 			}
+		}
+	}
+	if (KeyState[(unsigned char)'\x1b'] == INPUT_FIRST_PRESS)
+	{
+		if (menu == CONTROL)
+		{
+			menu = MAIN;
+			selection = 0;
+			MenuUpdate();
 		}
 	}
 }
